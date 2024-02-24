8c2f61119038f9534cfc7d2e8e791d046f7463c6
Clayton Hotze <clayton@hotzcraft.com>
2024-02-24 16:35:12 -0700

    Move to yaml for definitions of jobs and enviroments

--- SLICE_DET.py
+++ SLICE_DET.py
@@ -13,8 +13,9 @@ if __name__ == "__main__":
     freeze_support()
     parser = argparse.ArgumentParser()
 
-    parser.add_argument('--env_yaml', default=r"C:\Users\red\Desktop\SpatialNetForge\enviroment_helper.yaml")
-    parser.add_argument('--job_yaml', default=r"C:\Users\red\Desktop\SpatialNetForge\job.yaml")
+    parser.add_argument('--env_yaml', default=r"C:\Users\red\Desktop\SpatialNetForge\enviroment_helper.yaml",
+                        help="Path to Enviroment YAML")
+    parser.add_argument('--job_yaml', default=r"C:\Users\red\Desktop\SpatialNetForge\job.yaml", help="Path to Job YAML")
 
     args = parser.parse_args()
 
@@ -43,15 +44,25 @@ if __name__ == "__main__":
 
     sanity_check = True
     acceptable_files = ['.tif', '.tiff', '.geotiff', '.png', '.jpg', '.jpeg', '.bmp', '.sid']
+    sid_memory_adjustment = 64
+    tif_memory_adjustment = 3.6
+    small_file_adjustment = 64
     time_dict = {}
     time_dict['start'] = time.time()
-    files = [f for f in os.listdir(source_image_dir) if str(f).endswith(tuple(acceptable_files))]
+    files = [f for f in os.listdir(source_image_dir) if str(f).lower().endswith(tuple(acceptable_files))]
     list_predict_jobs = []
     num = 0
     # average file size of all files in the directory
     file_size = 0
     for file in files:
-        file_size += os.path.getsize(os.path.join(source_image_dir, file))
+        file_sz = os.path.getsize(os.path.join(source_image_dir, file))
+        if str(file).endswith(".sid"):
+            file_size += file_sz * sid_memory_adjustment
+        elif str(file).lower().endswith(".tiff") or str(file).lower().endswith(".tif") or str(file).lower().endswith(
+                "geotiff"):
+            file_size += file_sz * tif_memory_adjustment
+        else:
+            file_size += file_sz * small_file_adjustment
     file_size = file_size / len(files)
 
     if workers > 1 and sanity_check:
@@ -77,11 +88,8 @@ if __name__ == "__main__":
 
     for file in files:
         list_predict_jobs.append(
-            {'num': num, 'device': model_device, 'mrsid_exe': mrsid_decoder_path, 'detection_model': model_path,
-             'model_confidence_threshold': 0.5, 'input_dir': source_image_dir, 'input_name': file,
-             'slice_height': slice_height, 'slice_width': slice_width, 'overlap_height_ratio': overlap_height_ratio,
-             'overlap_width_ratio': overlap_width_ratio, 'export_dir': out_dir,
-             'file_type': ('sid' if file.endswith('.sid') else 'geotiff'), 'mt': mt, 'workers': workers})
+            {'num': num, 'job_config': job_config, 'env_config': env_config, 'input_name': file,
+             'file_type': ('sid' if file.endswith('.sid') else 'geotiff')})
         num += 1
 
     process_times = []
@@ -91,6 +99,23 @@ if __name__ == "__main__":
         with concurrent.futures.ProcessPoolExecutor(max_workers=workers) as executor:
             execs = executor.map(get_result, list_predict_jobs)
             for exec in execs:
+                if exec != None:
+                    if time_dict.get('predict') == None:
+                        time_dict['predict_times'] = [exec['time_dict']]
+                    else:
+                        time_dict['predict_times'].append(exec['time_dict'])
+                    time_dict_print = exec['time_dict']
+                    time_dict_print['start'] = time_dict['start']
+                    to_process -= 1
+                    process_times.append(exec['time_dict']['result_time'])
+                    ave_total = statistics.median(process_times)
+                    if exec['num'] % workers == 0:
+                        pass
+                    print_prediction_result(exec, time_dict_print, to_process, ave_total, workers)
+    else:
+        for job in list_predict_jobs:
+            exec = get_result(job)
+            if exec != None:
                 if time_dict.get('predict') == None:
                     time_dict['predict_times'] = [exec['time_dict']]
                 else:
@@ -100,22 +125,7 @@ if __name__ == "__main__":
                 to_process -= 1
                 process_times.append(exec['time_dict']['result_time'])
                 ave_total = statistics.median(process_times)
-                if exec['num'] % workers == 0:
-                    pass
                 print_prediction_result(exec, time_dict_print, to_process, ave_total, workers)
-    else:
-        for job in list_predict_jobs:
-            exec = get_result(job)
-            if time_dict.get('predict') == None:
-                time_dict['predict_times'] = [exec['time_dict']]
-            else:
-                time_dict['predict_times'].append(exec['time_dict'])
-            time_dict_print = exec['time_dict']
-            time_dict_print['start'] = time_dict['start']
-            to_process -= 1
-            process_times.append(exec['time_dict']['result_time'])
-            ave_total = statistics.median(process_times)
-            print_prediction_result(exec, time_dict_print, to_process, ave_total, workers)
     if merge_shps:
         poly_shps = [f for f in os.listdir(os.path.join(out_dir, 'shps', 'polys')) if f.endswith('.shp')]
         act_poly_shps = []
